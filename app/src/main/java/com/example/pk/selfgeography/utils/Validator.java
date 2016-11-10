3a44ed53089e064a623a093a8dbb1dd90f204529
Dmitriy Usic <usyasha69@mail.ru>
2016-11-10 12:52:46 +0200

    add content provider

--- app/src/main/java/com/example/pk/selfgeography/utils/Validator.java
+++ app/src/main/java/com/example/pk/selfgeography/utils/Validator.java
@@ -21,32 +21,7 @@ public class Validator {
      * @return - is validate
      */
     public boolean isValidatePassword(String password) {
-        int validate = 0;
-
-        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
-                , 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-
-        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-        for (char c : alphabet) {
-            if (password.contains(String.valueOf(c))) {
-                validate++;
-                break;
-            }
-        }
-
-        for (int number : numbers) {
-            if (password.contains(String.valueOf(number))) {
-                validate++;
-                break;
-            }
-        }
-
-        if (password.length() > 8) {
-            validate++;
-        }
-
-        return validate == 3;
+        return password.matches("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?!.*\\s).{8,}");
     }
 
     /**
